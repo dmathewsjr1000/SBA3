5ded3d407849e22a5cf00f3a46eed27560942dca
Dennis C. Mathews Jr. <dmathewsjr07106@gmail.com>
2024-05-05 21:13:00 -0400

    Files Updated

---
+++ Js/script.js
@@ -0,0 +1,38 @@
+// Declarining Variables
+const searchBtn = document.querySelector(".black-button");
+const inputField = document.querySelector(".number-input");
+const imageScreen = document.querySelector(".pokemon-picture");
+const nameScreen = document.getElementById("name");
+const height = document.getElementById("height");
+const weight = document.getElementById("weight");
+const type1 = document.getElementById("type-1");
+const type2 = document.getElementById("type-2");
+
+const resetFields = () => {
+    nameScreen.innerHTML = "";
+    height.innerHTML = "";
+    weight.innerHTML = "";
+    type1.innerHTML = "";
+    type2.innerHTML = "";
+  };
+
+
+async function  getPokemon(num) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`)
+    const data = await response.json();
+    imageScreen.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
+    nameScreen.innerHTML = data.name;
+    height.innerHTML = `${data.height * 10} cm`;
+    weight.innerHTML = `${data.weight / 10} kg`;
+    type1.innerHTML = data.types[0].type.name;
+    type2.innerHTML = data.types[1].type.name;
+}
+
+inputField.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      searchBtn.click();
+    }
+  });
+
+searchBtn.addEventListener('click', () => getPokemon(inputField.value));
+
