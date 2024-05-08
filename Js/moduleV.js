d916fa748ed1688b548d12a20262a2d43aab33d2
Dennis C. Mathews Jr. <dmathewsjr07106@gmail.com>
2024-05-08 16:02:44 -0400

    Files Upated

--- Js/moduleV.js
+++ Js/moduleV.js
@@ -1,5 +1,7 @@
-// Declarining Variables and function and the ones that are imported
-const searchBtn = document.querySelector(".black-button");
+// Declaring Variables and functions for exporting modules
+export const descreaseBtn = document.getElementById("decreaseBtn");
+export const resetBtn = document.getElementById("resetBtn");
+export const increaseBtn = document.getElementById("increaseBtn");
 const inputField = document.querySelector(".number-input");
 const imageScreen = document.querySelector(".pokemon-picture");
 const nameScreen = document.getElementById("name");
@@ -7,7 +9,6 @@ const height = document.getElementById("height");
 const weight = document.getElementById("weight");
 const type1 = document.getElementById("type-1");
 const type2 = document.getElementById("type-2");
-import { descreaseBtn, increaseBtn, increase, decrease } from "./moduleV.js";
 
 // Resetting function that reset fields on Pokedex
 const resetFields = () => {
@@ -20,25 +21,36 @@ const resetFields = () => {
 
 // Function that retreived data from api and convert to json data and displaying data
 async function getPokemon(num) {
+  resetFields();
   const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${num}`);
   const data = await response.json();
   imageScreen.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`;
   nameScreen.innerHTML = data.name.toUpperCase();
-  height.innerHTML = `${data.height * 10} cm`;
-  weight.innerHTML = `${data.weight / 10} kg`;
+  height.innerHTML = `${data.height * 10} CM`;
+  weight.innerHTML = `${data.weight / 10} KG`;
   type1.innerHTML = data.types[0].type.name.toUpperCase();
   type2.innerHTML = data.types[1].type.name.toUpperCase();
-  // type2.innerHTML !== false ? type2.innerHTML = data.types[1].type.name.toUpperCase(): type2.innerHTML = 'none'
 }
 
-// Event listener that catches enter keydown for other modes of entering data
-inputField.addEventListener("keydown", (e) => {
-  if (e.key === "Enter") {
-    searchBtn.click();
-  }
-});
+// Increasing Counter of the input data of pokemon id
+export function increase(num) {
+  inputField.value++;
+  console.log(inputField.value);
+  getPokemon(inputField.value);
+}
 
-// Event listeners that catches clicks on buttons three different buttons that activate a increasing poke id feature, decreasing poke id, and retreive data
-searchBtn.addEventListener("click", () => getPokemon(inputField.value));
-descreaseBtn.addEventListener("click", () => decrease(inputField.value));
-increaseBtn.addEventListener("click", () => increase(inputField.value));
+// Decreasing Counter of the input data of pokemon id
+export function decrease(num) {
+  inputField.value--;
+  if (inputField.value <= 0) {
+    alert("The pokedex does not have 0 or negative ids please enter a new id:");
+    reset(inputField.value);
+  }
+  getPokemon(inputField.value);
+}
+
+// Resetting Counter to the first pokemon id
+export function reset(num) {
+  inputField.value = 1;
+  getPokemon(inputField.value);
+}
